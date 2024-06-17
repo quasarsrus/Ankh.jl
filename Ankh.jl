00874e242b4c635e4f9e3e3c8163646c68eaa940
quasarsrus <56137267+quasarsrus@users.noreply.github.com>
2024-06-16 23:35:27 -0400

    Update Ankh.jl
    #10/8/2023, 23:25
    
    # 1) Added an argument "lambda" to "train_model". "lambda" was also added to "calculate_cost".
    # 2) "Forward_Propagation" also returns "sum_weights_sq".
    # 3) Argument "Weights_sum" was added to "calculate_cost".
    # 4) Added argument "lambda" to "backward_pass" and "back_propagation".
    
    #11/8/2023, 15:22
    
    # 1) Changed the parameter ordering in "train_model".
    # 2) Added compatibility for L1 and L2 regularisation by introducing a variable "n_reg".
    # 3) "n_reg" is also added as a parameter to forward propagation.

--- Ankh.jl
+++ Ankh.jl
@@ -1,5 +1,6 @@
 module Ankh
 
+using Pkg
 using Shuffle
 using Distributions
 using Statistics
@@ -40,7 +41,7 @@ end
 
 function activation_function(Z::Matrix{Float64}, name::String)
     
-    @assert name ∈ ("Sigmoid", "ReLu","tanh","LReLu","Linear")
+    @assert name ∈ ("Sigmoid", "ReLu","tanh","LReLu","Linear", "Swish")
     
     if name == "Sigmoid"
         A = 1 ./ (1 .+ exp.(.-Z))
@@ -57,6 +58,9 @@ function activation_function(Z::Matrix{Float64}, name::String)
     elseif name == "Linear"
         A = Z
         return (A = A, Z = Z)                
+    elseif name == "Swish"
+        A = Z * (1 ./ (1 .+ exp.(.-Z)))
+        return (A = A, Z = Z)
     end
 end
 
@@ -67,37 +71,39 @@ function forward_pass(A::Any, W::Matrix{Float64}, b::Matrix{Float64})
     return (Z = Z, cache = cache)
 end
 
-function forward_propagation(input::Any, parameters::Any,activation_type::Vector{String})
+function forward_propagation(input::Any, parameters::Any,activation_type::Vector{String}; n_reg::Int64 = 1)
     layer_size = Int(length(parameters) / 2)
     @assert layer_size ∈ (length(activation_type))
     master_cache = []
     A = input
+    sum_weights = 0;
     for i = 1 : (layer_size)
         A_prev = A
         A_trans,cache = forward_pass(A_prev, parameters[string("W", (i))], parameters[string("b", (i))])
         A,A_prev = activation_function(A_trans, activation_type[i])
         push!(master_cache , (linear_step_cache = cache, activation_step_cache = A_prev))
+        sum_weights = sum_weights + sum((parameters[string("W", (i))].^2).^(1/(n_reg*2%3)))
     end
     Ŷ = A
-    return Ŷ, master_cache
+    return Ŷ, master_cache, sum_weights
 end
 
-function calculate_cost(Ŷ::Any, Y::Any,cost_fcn::String)
+function calculate_cost(Ŷ::Any, Y::Any,cost_fcn::String, λ, ∑w)
     
     if cost_fcn == "Mean_Squared_Error" || cost_fcn == "mse"
-        mse = mean((Y - Ŷ).^2) 
+        mse = mean((Y - Ŷ).^2) + 0.5*λ*∑w;
         return mse
     end
     
     if cost_fcn == "binary_cross_entropy"
         epsilon = 1e-8
-        bce = -(sum(Y .* log.(Ŷ .+ epsilon) .+ (1 .- Y) .* log.(1 .- Ŷ .+ epsilon))) / size(Y)[2]
+        bce = -(sum(Y .* log.(Ŷ .+ epsilon) .+ (1 .- Y) .* log.(1 .- Ŷ .+ epsilon))) / size(Y)[2] + 0.5*λ*∑w;
         return bce
     end
 end
 
 function derivative_activation(Z::Matrix{Float64}, name::String)
-    @assert name ∈ ("Sigmoid", "ReLu","tanh","LReLu","Linear")
+    @assert name ∈ ("Sigmoid", "ReLu","tanh","LReLu","Linear","Swish")
     if name == "Sigmoid"
         temp = 1 ./ (1 .+ exp.(.-Z))
         A = temp .* (1 .- temp)
@@ -118,7 +124,11 @@ function derivative_activation(Z::Matrix{Float64}, name::String)
     elseif name == "Linear"
         A = ones(size(Z))
         return A              
-    end
+    elseif name == "Swish"
+        temp = 1 ./ (1 .+ exp.(.-Z))
+        A = temp + Z * (temp .* (1 .- temp))
+        return A
+    end    
 end
 
 function output_loss_derivative(Ŷ::Any, Y::Any, cost_fcn::String)
@@ -131,17 +141,21 @@ function output_loss_derivative(Ŷ::Any, Y::Any, cost_fcn::String)
       epsilon = 1e-8
       return (Ŷ .- Y) ./ (Ŷ .*(1 .- Ŷ) .+ epsilon)
     end
-end         
+end                                  
 
-function backward_pass(∂E, A, W)
+function backward_pass(∂E, A, W, λ::Float64, n_reg::Int64)
     m = size(A, 1)
-    ∂W = (A'* ∂E) ./ m
+    
+    ∂W = (A'* ∂E) ./ m + (0.5)^(n_reg%2)*λ*(W'./((W'.^(n_reg*2%4)).^(1/2)))
     ∂b = (sum(∂E,dims=1)) ./ m
     ∂A = ∂E * W
+    
     return (∂W,∂b,∂A)
 end
 
-function back_propagation(Ŷ::Any, Y::Any, master_cache::Any, activation_type::Vector{String}, loss_fcn::String)
+function back_propagation(Ŷ::Any, Y::Any, master_cache::Any, activation_type::Vector{String}, 
+        loss_fcn::String, λ::Float64, n_reg::Int64)
+    
     ∇ = Dict()
     linear_cache, activation_cache = master_cache[end]
     ∂E = derivative_activation(activation_cache, activation_type[end]) .* output_loss_derivative(Ŷ, Y, loss_fcn)
@@ -149,7 +163,7 @@ function back_propagation(Ŷ::Any, Y::Any, master_cache::Any, activation_type::
         linear_cache,_ = master_cache[i]
 
         ∇[string("∂W",(i))], ∇[string("∂b",(i))], ∇[string("∂A",(i))] = 
-                                backward_pass(∂E, linear_cache[1],linear_cache[2])
+                                backward_pass(∂E, linear_cache[1],linear_cache[2], λ, n_reg)
 
         if i > 1
             _, activation_cache = master_cache[i-1]
@@ -196,13 +210,14 @@ function update_weights(params,∇::Any, learning_rate, network_size ,optimiser)
 end
 
 function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int64}, activation_type::Vector{String}, 
-        loss_fcn::String; lr::Float64=0.01,epochs::Int64= 1,verbose::Bool=true, shuffle_data::Bool = true, 
-        steps_per_epoch = nothing, batch_size = nothing, optimiser::String = "gradient_descent")
+        loss_fcn::String; lr::Float64=0.001, epochs::Int64= 1, verbose::Bool=true, optimiser::String = "gradient_descent",
+        shuffle_data::Bool = true, lambda::Float64 = 0, n_reg::Int64 = 1, steps_per_epoch = nothing, batch_size = nothing, 
+        log::Bool = false, log_file::String = "Loss_Log")
     
     @assert optimiser ∈ ("gradient_descent", "stochastic_gradient_descent", "gradient_descent_with_momentum", "rmsprop", "Adam")
     
     if batch_size == nothing && steps_per_epoch == nothing
-      batch_size = Int(ceil(x_train.shape[1] / 2))
+      batch_size = Int(ceil(size(x_train)[1] / 2))
       steps_per_epoch = Int(ceil(size(x_train)[1]/batch_size))
       
     elseif batch_size == nothing && steps_per_epoch != nothing
@@ -211,7 +226,7 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
     elseif batch_size != nothing && steps_per_epoch == nothing
       steps_per_epoch = Int(ceil(size(x_train)[1]/batch_size))
     end
-    
+
     if optimiser == "gradient_descent"
         
         shuffled_index = [i for i = 1:size(x_train)[1]]
@@ -221,12 +236,25 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
             if shuffle_data
                 shuffle!(shuffled_index)
             end              
-            Ŷ, cache = forward_propagation(x_train[shuffled_index[:],:], params, activation_type)
-            loss = calculate_cost(Ŷ, y_train[shuffled_index[:],:], loss_fcn)
-            ∇ = back_propagation(Ŷ, y_train[shuffled_index[:],:], cache, activation_type, loss_fcn)
+            Ŷ, cache, weights_sum = forward_propagation(x_train[shuffled_index[:],:], params, activation_type; n_reg)
+            loss = calculate_cost(Ŷ, y_train[shuffled_index[:],:], loss_fcn, lambda, weights_sum)
+            ∇ = back_propagation(Ŷ, y_train[shuffled_index[:],:], cache, activation_type, loss_fcn, lambda, n_reg)
             params = update_weights(params,∇,lr,length(layer_dims),optimiser)
             if verbose
                 println("Iteration -> $i, Cost -> $loss")
+            end
+            
+            if log
+                open(string(log_file,".txt"), "a") do file
+                    write(file, string(loss))
+                    write(file, "\n")
+                end
+            end 
+        end
+        if log
+            open(string(log_file,".txt"), "a") do file
+                write(file, "*******")
+                write(file, "\n")
             end
         end
     end
@@ -239,15 +267,27 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
             loss = 0
             shuffle!(shuffled_index)
             for j = 1:length(shuffled_index)
-                Ŷ, cache = forward_propagation(x_train[shuffled_index[j],:], params, activation_type)
-                loss = calculate_cost(Ŷ, y_train[shuffled_index[j],:], loss_fcn)
-                ∇ = back_propagation(Ŷ, y_train[shuffled_index[j],:], cache, activation_type, loss_fcn)
+                Ŷ, cache, weights_sum = forward_propagation(x_train[shuffled_index[j],:], params, activation_type; n_reg)
+                loss = calculate_cost(Ŷ, y_train[shuffled_index[j],:], loss_fcn, lambda, weights_sum)
+                ∇ = back_propagation(Ŷ, y_train[shuffled_index[j],:], cache, activation_type, loss_fcn, lambda, n_reg)
                 params = update_weights(params,∇,lr,length(layer_dims),optimiser)
             end
             if verbose
                 println("Iteration -> $i, Cost -> $loss")
             end
             
+            if log
+                open(string(log_file,".txt"), "a") do file
+                    write(file, string(loss))
+                    write(file, "\n")
+                end
+            end 
+        end
+        if log
+            open(string(log_file,".txt"), "a") do file
+                write(file, "*******")
+                write(file, "\n")
+            end
         end
     end
     
@@ -271,9 +311,11 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
             
             for k = 1:steps_per_epoch
 
-                Ŷ, cache = forward_propagation(x_train[shuffled_index[marker:batch],:], params, activation_type)
-                loss = calculate_cost(Ŷ, y_train[shuffled_index[marker:batch],:], loss_fcn)            
-                ∇ = back_propagation(Ŷ, y_train[shuffled_index[marker:batch],:], cache, activation_type, loss_fcn)
+                Ŷ, cache, weights_sum = forward_propagation(x_train[shuffled_index[marker:batch],:], params, 
+                    activation_type; n_reg)
+                loss = calculate_cost(Ŷ, y_train[shuffled_index[marker:batch],:], loss_fcn, lambda, weights_sum)            
+                ∇ = back_propagation(Ŷ, y_train[shuffled_index[marker:batch],:], cache, activation_type, 
+                    loss_fcn, lambda, n_reg)
 
                 for j = 1:length(layer_dims)-1
                     momentum[string("v_dw",j)] = β .* momentum[string("v_dw",j)] .+ ((1 .- β) .* ∇[string("∂W",j)])'
@@ -298,6 +340,18 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
                 println("Iteration -> $i, Cost -> ", loss/steps_per_epoch)
             end
             
+            if log
+                open(string(log_file,".txt"), "a") do file
+                    write(file, string(loss/steps_per_epoch))
+                    write(file, "\n")
+                end
+            end 
+        end
+        if log
+            open(string(log_file,".txt"), "a") do file
+                write(file, "*******")
+                write(file, "\n")
+            end
         end
     end
                 
@@ -321,9 +375,11 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
             
             for k = 1:steps_per_epoch
                 
-                Ŷ, cache = forward_propagation(x_train[shuffled_index[marker:batch],:], params, activation_type)
-                loss = calculate_cost(Ŷ, y_train[shuffled_index[marker:batch],:], loss_fcn)            
-                ∇ = back_propagation(Ŷ, y_train[shuffled_index[marker:batch],:], cache, activation_type, loss_fcn)
+                Ŷ, cache, weights_sum = forward_propagation(x_train[shuffled_index[marker:batch],:], params, 
+                    activation_type; n_reg)
+                loss = calculate_cost(Ŷ, y_train[shuffled_index[marker:batch],:], loss_fcn, lambda, weights_sum)            
+                ∇ = back_propagation(Ŷ, y_train[shuffled_index[marker:batch],:], cache, activation_type, 
+                    loss_fcn, lambda, n_reg)
 
                 for j = 1:length(layer_dims)-1
                     rms[string("s_dw",j)] = β .* rms[string("s_dw",j)] .+ ((1 .- β) .* (∇[string("∂W",j)]) .^ 2)'
@@ -348,6 +404,18 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
                 println("Iteration -> $i, Cost -> ", loss/steps_per_epoch)
             end
             
+            if log
+                open(string(log_file,".txt"), "a") do file
+                    write(file, string(loss/steps_per_epoch))
+                    write(file, "\n")
+                end
+            end 
+        end
+        if log
+            open(string(log_file,".txt"), "a") do file
+                write(file, "*******")
+                write(file, "\n")
+            end
         end
     end
     
@@ -374,9 +442,11 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
             
             for k = 1:steps_per_epoch
                 
-                Ŷ, cache = forward_propagation(x_train[shuffled_index[marker:batch],:], params, activation_type)
-                loss = calculate_cost(Ŷ, y_train[shuffled_index[marker:batch],:], loss_fcn)            
-                ∇ = back_propagation(Ŷ, y_train[shuffled_index[marker:batch],:], cache, activation_type, loss_fcn)
+                Ŷ, cache, weights_sum = forward_propagation(x_train[shuffled_index[marker:batch],:], params, 
+                    activation_type; n_reg)
+                loss = calculate_cost(Ŷ, y_train[shuffled_index[marker:batch],:], loss_fcn, lambda, weights_sum)            
+                ∇ = back_propagation(Ŷ, y_train[shuffled_index[marker:batch],:], cache, activation_type, 
+                    loss_fcn, lambda, n_reg)
                 
                 for j = 1:length(layer_dims)-1
                     adam_opt[string("v_dw",j)] = β1 .* adam_opt[string("v_dw",j)] .+ (1 - β1) .* ∇[string("∂W",j)]'
@@ -410,10 +480,24 @@ function train_model(params, x_train::Any, y_train::Any, layer_dims::Vector{Int6
                 println("Iteration -> $i, Cost -> ", loss/steps_per_epoch)
             end
             
+            if log
+                open(string(log_file,".txt"), "a") do file
+                    write(file, string(loss/steps_per_epoch))
+                    write(file, "\n")
+                end
+            end                
+        end
+        if log
+            open(string(log_file,".txt"), "a") do file
+                write(file, "*******")
+                write(file, "\n")
+            end
         end
     end
     
     return params
 end
 
-end
+
+
+
